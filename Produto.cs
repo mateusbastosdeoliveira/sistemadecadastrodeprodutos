d34380b63db93a4d0f70a37142e208278e9be39b
Mateus Bastos <105644793+mateusbastosdeoliveira@users.noreply.github.com>
2024-11-26 15:51:55 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SisteDeCadastroDeProdutos
+{
+    internal class Produto
+    {   
+        private String? _nome;
+        //private double _preco;
+        private String? _quantidade;
+
+
+
+
+        private int _totalProduto = 0;
+        List<string> _listaprodutos = new List<string> { };
+
+        public Produto(String nome, String quantidade) {
+        this._nome = nome;
+        // this._preco = preco;
+        this._quantidade = quantidade;
+        
+        
+        }
+
+        public void adicionarProduto(String produto, String quantidade) {
+            if (produto != null && produto != "" && quantidade != null && quantidade != "")
+            {
+                if (_listaprodutos.Contains(produto)) {
+                     
+                    try
+                    {
+                        int quantidadeC = Convert.ToInt32(quantidade);
+                        _totalProduto += quantidadeC;
+                            Console.WriteLine($"Produto {produto} adiconado na lista, contém o total de {_totalProduto}.");
+                            Console.WriteLine();
+                        
+                   
+                           
+                    }
+                    catch(FormatException){ 
+                        Console.WriteLine("Digite um valor válido");
+                        Console.WriteLine();
+                    }
+                
+
+
+                }
+                else{
+                    try
+                    {
+                        int quantidadeC = Convert.ToInt32(quantidade);
+                        _totalProduto += quantidadeC;
+                        _listaprodutos.Add(produto);
+                        Console.WriteLine($"Produto {produto} com a quantidade de {_totalProduto} adiconado na lista ");
+                        Console.WriteLine();
+
+
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Digite um valor válido");
+                        Console.WriteLine();
+                    }
+                    
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Produto ou quantidade não podem ser vázio");
+                Console.WriteLine();
+            }
+
+
+        
+        }
+
+        public void listarProdutos()
+        {
+            if(_listaprodutos.Count == 0) { Console.WriteLine("Lista vázia");
+                Console.WriteLine();
+            }
+            else { 
+            Console.WriteLine("Listando produtos...");
+            foreach (String p in _listaprodutos) {
+                
+                Console.WriteLine($"{p}");
+                Console.WriteLine();
+                
+                }
+            }
+        }
+
+
+        public void removerProduto(String produto, String? quantidade) {
+
+            if (produto != null && _listaprodutos.Contains(produto)) {
+                    try
+                    {
+                        int quantidadeC = Convert.ToInt32(quantidade);
+                
+                            _totalProduto -= quantidadeC;
+
+                    if (_totalProduto <= 0)
+                    {
+                        Console.WriteLine("Quantidade de produto zerada, ou valor digitado  para remoção era maior que a quantidade que continha");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Produto {produto} removido da lista, restam {_totalProduto}");
+                        Console.WriteLine();
+                    }
+
+
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Digite um valor válido para remoção");
+                        Console.WriteLine();
+                    }
+   
+            }
+
+            else
+            {
+                Console.WriteLine("Produto não foi localizado ou você digitou um valor em branco.");
+                Console.WriteLine();
+              
+            }
+
+
+        }
+
+
+
+        public bool ExisteProduto(string produto)
+        {
+            return _listaprodutos.Contains(produto);
+        }
+
+
+        public void consultarProduto(String produto) {
+            if (_listaprodutos.Contains(produto)) { Console.WriteLine($"Produto {produto} existe na lista");
+                Console.WriteLine();
+            }
+            else { Console.WriteLine($"Produto {produto} não existe na lista");
+                Console.WriteLine();
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿namespace SistemaDeCadastroDeProdutos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema de Cadastro");
+            Console.WriteLine();
+
+            bool emExecucao = true;
+            SisteDeCadastroDeProdutos.Produto p = new SisteDeCadastroDeProdutos.Produto("", "" );
+
+            while (emExecucao)
+            {
+                Console.WriteLine("""
+                    Selecione uma opção
+                    0 - Sair
+                    1 - Adicionar produto
+                    2 - Listar produtos
+                    3 - Remover produto
+                    4 - Consultar produto
+                    """);
+                Console.WriteLine();
+                String opcao = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (opcao)
+                {
+
+                    case "0":
+                        Console.WriteLine("Saindo..");
+                        emExecucao = false;
+                        break;
+
+                    case "1":
+                        Console.WriteLine("Descreva o produto");
+                        String produto = Console.ReadLine().ToUpper();
+                        Console.WriteLine("Descreva a quantidade do produto");
+                        String quantidade = Console.ReadLine();
+                        p.adicionarProduto(produto, quantidade);
+                        break;
+
+                    case "2":
+                        p.listarProdutos();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Digite o produto para ser removido");
+                        String produtoRemover = Console.ReadLine().ToUpper();
+
+                        if (!p.ExisteProduto(produtoRemover))
+                        {
+                            Console.WriteLine($"Produto {produtoRemover} não encontrado na lista.");
+                            Console.WriteLine();
+                            continue;
+                            
+                        }
+
+
+                        Console.WriteLine("Digite a quantidade do produto a ser removido");
+                        String quantidadeRemover = Console.ReadLine();
+                        p.removerProduto(produtoRemover, quantidadeRemover);
+                        break;
+                    case "4":
+                        Console.WriteLine("Digite o produto para consulta");
+                        String produtoConsultar = Console.ReadLine().ToUpper();
+                        p.consultarProduto(produtoConsultar);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
