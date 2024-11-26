93cc744d4d4ff7fea55963c65d99214cbebbc599
Mateus Bastos <105644793+mateusbastosdeoliveira@users.noreply.github.com>
2024-11-26 18:52:08 -0300

    Atualizando os códigos fontes

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@
                 Console.WriteLine();
                 String opcao = Console.ReadLine();
                 Console.WriteLine();
+                Console.Clear();
 
                 switch (opcao)
                 {
@@ -35,33 +36,38 @@
                     case "1":
                         Console.WriteLine("Descreva o produto");
                         String produto = Console.ReadLine().ToUpper();
+                        Console.Clear();
                         Console.WriteLine("Descreva a quantidade do produto");
                         String quantidade = Console.ReadLine();
+                        Console.Clear();
                         p.adicionarProduto(produto, quantidade);
                         break;
 
                     case "2":
+                        Console.Clear();
                         p.listarProdutos();
                         break;
 
                     case "3":
+                        Console.Clear();
                         Console.WriteLine("Digite o produto para ser removido");
                         String produtoRemover = Console.ReadLine().ToUpper();
-
                         if (!p.ExisteProduto(produtoRemover))
                         {
+                            Console.Clear();
                             Console.WriteLine($"Produto {produtoRemover} não encontrado na lista.");
                             Console.WriteLine();
                             continue;
                             
                         }
 
-
+                        Console.Clear();
                         Console.WriteLine("Digite a quantidade do produto a ser removido");
                         String quantidadeRemover = Console.ReadLine();
                         p.removerProduto(produtoRemover, quantidadeRemover);
                         break;
                     case "4":
+                        Console.Clear();
                         Console.WriteLine("Digite o produto para consulta");
                         String produtoConsultar = Console.ReadLine().ToUpper();
                         p.consultarProduto(produtoConsultar);
